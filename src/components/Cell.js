e98100f3200475f2ed6fcd6d4a772f0d0ecf06d7
Luke <luke@Johnnys-iMac.local>
2019-08-16 17:52:53 -0700

    Add basic styles as styled components

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { StyledCell } from './styles/StyledCell';
+import { TETROMINOES } from '../tetrominoes';
 
 const Cell = ({ type }) => (
-	<div>cell</div>
+	<StyledCell type={type} color={TETROMINOES[type].color}/>
 )
 
 export default Cell;
